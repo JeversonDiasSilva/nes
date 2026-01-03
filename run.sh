2de226954564ffb928745f33532fbd969be7c0a2
Jeverson Dias da Silva <jeversondiasdasilva@gmail.com>
2026-01-03 04:45:53 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -302,7 +302,7 @@ elif [ "$VERSAO" -eq 42 ]; then
     echo "--- Script V5: Bloqueio Total de Notificações ---"
 
     # 1. Permite escrita
-    mount -o remount,rw /
+    # mount -o remount,rw /
 
     # 2. Backups (Se não existirem)
     [ ! -f "$ARQUIVO_CONFIG.bak" ] && cp "$ARQUIVO_CONFIG" "$ARQUIVO_CONFIG.bak"
